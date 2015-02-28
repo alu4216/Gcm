8cb8225efee42857598872e699011b245db5437b
Josué <josss9229@gmail.com>
2015-02-28 11:53:40 +0000

    Ejemplo de aplicación sobre GCM (Google Cloud Messaging)
    Aplicación de prueba y aprendizaje sobre el servicioe GCM (Google Cloud
    Messaging)

---
+++ src/com/josue/gcm/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.josue.gcm;
+
+public interface ApplicationConstants {
+	 
+    // Php Application URL to store Reg ID created
+    static final String APP_SERVER_URL = "http://192.168.0.10:80/gcm/gcm.php?shareRegId=true";
+ 
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "309830543870";
+    // Message Key
+    static final String MSG_KEY = "m";
+ 
+}
